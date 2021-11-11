1b9179edb54cee3a9fdcf1e7098de6459af9a221
MrCringeGuy <93745239+MrCringeGuy@users.noreply.github.com>
2021-11-11 23:46:43 +0000

    Add files via upload

---
+++ vgen2.py
@@ -0,0 +1,68 @@
+import random
+import pyautogui
+import pynput
+import webbrowser
+import time
+
+from pynput.keyboard import Key, Controller
+
+keyboard = Controller()
+
+webbrowser.open('https://www.epicgames.com/fortnite/en-US/vbuckscard')
+
+while True:
+
+    codeChar = random.choice(["A", "B", "C", "D", "E", "F", "G", "H", "I", "J", "K", "L", "M", "N", "O", "P", "Q", "R", "S", "T", "U", "V", "W", "X", "Y", "Z", "1", "2", "3", "4", "5", "6", "7", "8", "9", "0"])
+    C1 = codeChar
+    codeChar = random.choice(["A", "B", "C", "D", "E", "F", "G", "H", "I", "J", "K", "L", "M", "N", "O", "P", "Q", "R", "S", "T", "U", "V", "W", "X", "Y", "Z", "1", "2", "3", "4", "5", "6", "7", "8", "9", "0"])
+    C2 = codeChar
+    codeChar = random.choice(["A", "B", "C", "D", "E", "F", "G", "H", "I", "J", "K", "L", "M", "N", "O", "P", "Q", "R", "S", "T", "U", "V", "W", "X", "Y", "Z", "1", "2", "3", "4", "5", "6", "7", "8", "9", "0"])
+    C3 = codeChar
+    codeChar = random.choice(["A", "B", "C", "D", "E", "F", "G", "H", "I", "J", "K", "L", "M", "N", "O", "P", "Q", "R", "S", "T", "U", "V", "W", "X", "Y", "Z", "1", "2", "3", "4", "5", "6", "7", "8", "9", "0"])
+    C4 = codeChar
+    codeChar = random.choice(["A", "B", "C", "D", "E", "F", "G", "H", "I", "J", "K", "L", "M", "N", "O", "P", "Q", "R", "S", "T", "U", "V", "W", "X", "Y", "Z", "1", "2", "3", "4", "5", "6", "7", "8", "9", "0"])
+    C5 = codeChar
+    codeChar = random.choice(["A", "B", "C", "D", "E", "F", "G", "H", "I", "J", "K", "L", "M", "N", "O", "P", "Q", "R", "S", "T", "U", "V", "W", "X", "Y", "Z", "1", "2", "3", "4", "5", "6", "7", "8", "9", "0"])
+    C6 = codeChar
+    codeChar = random.choice(["A", "B", "C", "D", "E", "F", "G", "H", "I", "J", "K", "L", "M", "N", "O", "P", "Q", "R", "S", "T", "U", "V", "W", "X", "Y", "Z", "1", "2", "3", "4", "5", "6", "7", "8", "9", "0"])
+    C7 = codeChar
+    codeChar = random.choice(["A", "B", "C", "D", "E", "F", "G", "H", "I", "J", "K", "L", "M", "N", "O", "P", "Q", "R", "S", "T", "U", "V", "W", "X", "Y", "Z", "1", "2", "3", "4", "5", "6", "7", "8", "9", "0"])
+    C8 = codeChar
+    codeChar = random.choice(["A", "B", "C", "D", "E", "F", "G", "H", "I", "J", "K", "L", "M", "N", "O", "P", "Q", "R", "S", "T", "U", "V", "W", "X", "Y", "Z", "1", "2", "3", "4", "5", "6", "7", "8", "9", "0"])
+    C9 = codeChar
+    codeChar = random.choice(["A", "B", "C", "D", "E", "F", "G", "H", "I", "J", "K", "L", "M", "N", "O", "P", "Q", "R", "S", "T", "U", "V", "W", "X", "Y", "Z", "1", "2", "3", "4", "5", "6", "7", "8", "9", "0"])
+    C10 = codeChar
+    codeChar = random.choice(["A", "B", "C", "D", "E", "F", "G", "H", "I", "J", "K", "L", "M", "N", "O", "P", "Q", "R", "S", "T", "U", "V", "W", "X", "Y", "Z", "1", "2", "3", "4", "5", "6", "7", "8", "9", "0"])
+    C11 = codeChar
+    codeChar = random.choice(["A", "B", "C", "D", "E", "F", "G", "H", "I", "J", "K", "L", "M", "N", "O", "P", "Q", "R", "S", "T", "U", "V", "W", "X", "Y", "Z", "1", "2", "3", "4", "5", "6", "7", "8", "9", "0"])
+    C12 = codeChar
+    codeChar = random.choice(["A", "B", "C", "D", "E", "F", "G", "H", "I", "J", "K", "L", "M", "N", "O", "P", "Q", "R", "S", "T", "U", "V", "W", "X", "Y0KVRGE18CWS26Q6G", "Z", "1", "2", "3", "4", "5", "6", "7", "8", "9", "0"])
+    C13 = codeChar
+    codeChar = random.choice(["A", "B", "C", "D", "E", "F", "G", "H", "I", "J", "K", "L", "M", "N", "O", "P", "Q", "R", "S", "T", "U", "V", "W", "X", "Y", "Z", "1", "2", "3", "4", "5", "6", "7", "8", "9", "0"])
+    C14 = codeChar
+    codeChar = random.choice(["A", "B", "C", "D", "E", "F", "G", "H", "I", "J", "K", "L", "M", "N", "O", "P", "Q", "R", "S", "T", "U", "V", "W", "X", "Y", "Z", "1", "2", "3", "4", "5", "6", "7", "8", "9", "0"])
+    C15 = codeChar
+    codeChar = random.choice(["A", "B", "C", "D", "E", "F", "G", "H", "I", "J", "K", "L", "M", "N", "O", "P", "Q", "R", "S", "T", "U", "V", "W", "X", "YF4SJBZYQVJD9JZ0W", "Z", "1", "2", "3", "4", "5", "6", "7", "8", "9", "0"])
+    C16= codeChar
+    codeChar = random.choice(["A", "B", "C", "D", "E", "F", "G", "H", "I", "J", "K", "L", "M", "N", "O", "P", "Q", "R", "S", "T", "U", "V", "W", "X", "Y", "Z", "1", "2", "3", "4", "5", "6", "7", "8", "9", "0"])
+
+    code = (C1, C2, C3, C4, C5, C6, C7, C8, C9, C10, C11, C12, C13, C14, C15, C16)
+
+    time.sleep(2.5)
+    pyautogui.click(1200, 660)
+    time.sleep(1)
+    pyautogui.click(1225, 600)
+    time.sleep(0.1)
+    keyboard.type(code)
+    time.sleep(0.1)
+    pyautogui.click(1130, 680)
+    time.sleep(1)
+    if pyautogui.pixel(1058, 599) == (255, 0, 0):
+        pyautogui.click(100, 70)
+    else:
+        print("Succesful code: ", code)
+        pyautogui.click(100, 70)
+        file = open("Valid.txt", "a")
+        file.write("Code = ", code)
+        file.write("\n")
+    time.sleep(1.5)
